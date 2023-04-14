e57e087fd9fa7380f8ee1c1df1c3185b8d82997f
David McHealy <david@simplecast.com>
2023-04-14 10:56:59 -0400

    Forgot to add these.

---
+++ mix.exs
@@ -0,0 +1,54 @@
+defmodule Simpleramix.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :simpleramix,
+      version: "1.0.0",
+      elixir: "~> 1.12",
+      start_permanent: Mix.env() == :prod,
+      description: "Client library for sending requests to Druid.",
+      source_url: "https://github.com/GameAnalytics/simpleramix",
+      package: package(),
+      deps: deps(),
+
+      # Docs
+      name: "Simpleramix",
+      source_url: "https://github.com/GameAnalytics/simpleramix",
+      homepage_url: "https://github.com/GameAnalytics/simpleramix",
+      docs: [
+        # The main page in the docs
+        main: "Simpleramix",
+        extras: ["README.md"]
+      ]
+    ]
+  end
+
+  defp package do
+    [
+      files: ["config", "lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Magnus Henoch"],
+      licenses: ["Apache-2.0"],
+      links: %{github: "https://github.com/GameAnalytics/simpleramix"}
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:jason, "~> 1.1"},
+      {:httpoison, "~> 1.0"},
+      {:timex, "~> 3.1"},
+      {:dialyxir, "~> 1.0-rc.3", only: [:dev], runtime: false},
+      {:credo, "~> 1.6.5", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.27.3", only: :dev, runtime: false}
+    ]
+  end
+end
