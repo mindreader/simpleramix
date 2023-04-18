5386dddbeecee15a9c781f1d7400facf5364e515
David McHealy <david@simplecast.com>
2023-04-18 09:27:24 -0400

    Tagging a release.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Simpleramix.MixProject do
   def project do
     [
       app: :simpleramix,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       description: "Client library for sending requests to Druid.",
