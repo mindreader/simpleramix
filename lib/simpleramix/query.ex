bd00216b1926582acce3faaec252de0a393f36fd
David McHealy <david@simplecast.com>
2023-04-19 15:17:56 -0400

    Turns out druid doesn't interpret strings as dates.

--- lib/simpleramix/query.ex
+++ lib/simpleramix/query.ex
@@ -647,7 +647,7 @@ defmodule Simpleramix.Query do
             {Float.to_string(l), Float.to_string(u), :numeric}
 
           {%DateTime{} = l, %DateTime{} = u} ->
-            {DateTime.to_iso8601(l), DateTime.to_iso8601(u), :numeric}
+            {DateTime.to_unix(l), DateTime.to_unix(u), :numeric}
 
           {l, u} when is_binary(l) and is_binary(u) ->
             {l, u, :lexicographic}
